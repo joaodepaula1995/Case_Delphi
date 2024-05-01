9494d5863299888e73636043a23caac30d1f8e53
joaodepaula1995 <joaodepaula1995@hotmail.com>
2024-05-01 15:44:33 -0300

    Elaboração do Modelo/Tela do Menu Principal
    Elaboração do Modelo do Menu Principal;
    Elaboração da Tela FRM_MENU_PRINCIPAL;

--- Software/Case_Delphi.dpr
+++ Software/Case_Delphi.dpr
@@ -2,8 +2,12 @@ program Case_Delphi;
 
 uses
   Vcl.Forms,
-  unitLogin in 'unitLogin.pas' {FRM_LOGIN},
-  unit_Fecha_Sistema in 'unit_Fecha_Sistema.pas' {FRM_FECHA_SISTEMA};
+  unit_Login in 'unit_Login.pas' {FRM_LOGIN},
+  unit_Fecha_Sistema in 'unit_Fecha_Sistema.pas' {FRM_FECHA_SISTEMA},
+  unit_Mensagem in 'unit_Mensagem.pas' {FRM_MENSAGEM},
+  unitDM_Login in 'unitDM_Login.pas' {DM_Login: TDataModule},
+  unitSQL_Login in 'unitSQL_Login.pas',
+  unit_Menu_Principal in 'unit_Menu_Principal.pas' {FRM_MENU_PRINCIPAL};
 
 {$R *.res}
 
@@ -12,5 +16,8 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFRM_LOGIN, FRM_LOGIN);
   Application.CreateForm(TFRM_FECHA_SISTEMA, FRM_FECHA_SISTEMA);
+  Application.CreateForm(TFRM_MENSAGEM, FRM_MENSAGEM);
+  Application.CreateForm(TDM_Login, DM_Login);
+  Application.CreateForm(TFRM_MENU_PRINCIPAL, FRM_MENU_PRINCIPAL);
   Application.Run;
 end.
